124793c4701cb4d0c9caf9d41b56d74d09ffe924
Manivannan Guru <manivannan.g@manurevasolutions.net>
2017-11-14 18:45:13 +0530

    first push

--- sampleexp.js
+++ sampleexp.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/sampleexp', function (req, res) {
-    var response = 'hello world!!!';
+    var response = 'hello world 123!!!';
     //callback(null, response);
     res.send(response);
 });
